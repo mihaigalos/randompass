cd525084e97ed687c3fb68097c7e2937e53b36e5
Mihai Galos <mihaigalos@gmail.com>
2022-01-25 19:08:39 +0100

    Use clap function calls instead of macros

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,54 @@
-use clap::{clap_app, crate_description, crate_version};
+use clap::crate_version;
+use clap::{App, Arg};
 use randompass::alphabet::Alphabet;
 
 #[cfg(not(tarpaulin_include))]
 fn main() {
-    let cli_args = clap_app!(randompass =>
-        (version: crate_version!())
-        (about: crate_description!())
-        (author: "Mihai Galos <mihaigalos at gmail dot com>")
-            (@arg length:           +takes_value -l --length              "Password length.")
-            (@arg no_lowercase:                  -o --no_lowercase        "Disable usage of lowercase letters.")
-            (@arg no_numbers:                    -n --no_numbers          "Disable usage of numbers.")
-            (@arg no_special_chars:              -c --no_special_chars    "Disable usage of special characters (i.e.: !, $, #).")
-            (@arg no_uppercase:                  -u --no_uppercase        "Disable usage of uppercase letters.")
+    let cli_args = App::new(concat!(
+        env!("CARGO_CRATE_NAME"),
+        " ",
+        env!("CARGO_PKG_VERSION"),
+        " :: ",
+        concat!(
+            env!("CARGO_PKG_REPOSITORY"),
+            "/releases/tag/",
+            crate_version!()
         )
-        .get_matches_safe()
-        .unwrap_or_else(|e| e.exit());
+    ))
+    .author(env!("CARGO_PKG_AUTHORS"))
+    .about(env!("CARGO_PKG_DESCRIPTION"))
+    .arg(
+        Arg::with_name("length")
+            .long("length")
+            .short("l")
+            .help("Password length."),
+    )
+    .arg(
+        Arg::with_name("no_lowercase")
+            .long("no_lowercase")
+            .short("o")
+            .help("Disable usage of lowercase letters."),
+    )
+    .arg(
+        Arg::with_name("no_numbers")
+            .long("no_numbers")
+            .short("n")
+            .help("Disable usage of numbers."),
+    )
+    .arg(
+        Arg::with_name("no_special_chars")
+            .long("no_special_chars")
+            .short("c")
+            .help("Disable usage of special characters (i.e.: !, $, #)."),
+    )
+    .arg(
+        Arg::with_name("no_uppercase")
+            .long("no_uppercase")
+            .short("u")
+            .help("Disable usage of uppercase letters."),
+    )
+    .get_matches_safe()
+    .unwrap_or_else(|e| e.exit());
 
     let config = randompass::config::Configurator { cli_args };
     let pass = randompass::password::Password::generate(&config, Alphabet::new(&config));
